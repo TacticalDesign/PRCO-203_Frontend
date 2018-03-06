cd93a1cc5f6dd2fd91545668d46a26f05982a3b4
Toby Smith <tobysmith568@hotmail.co.uk>
2018-03-06 17:16:23 +0000

    Can swap between panels

---
+++ Assets/Scripts/MainCanvas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCanvas : MonoBehaviour
+{
+
+    List<GameObject> mainPanels = new List<GameObject>();
+
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).tag == "MainPanel")
+            {
+                mainPanels.Add(transform.GetChild(i).gameObject);
+                Debug.Log(transform.GetChild(i).name);
+            }
+        }
+    }
+
+    public void HideAllPanels()
+    {
+        foreach (GameObject panel in mainPanels)
+        {
+            panel.SetActive(false);
+        }
+    }
+}
