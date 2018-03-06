89418a29d7e3702545a0b407950b9f484526e214
Toby Smith <tobysmith568@hotmail.co.uk>
2018-03-06 21:33:29 +0000

    Added all main panels

--- Assets/Scripts/General/NavbarItem.cs
+++ Assets/Scripts/General/NavbarItem.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NavbarItem : MonoBehaviour
 {
+    [Header("Transition Settings")]
     [SerializeField]
     GameObject panel;
     [SerializeField]
     MainCanvas mainCanvas;
+    [Header("Title Settings")]
+    [SerializeField]
+    string titleMessage;
 
     /// <summary>
     /// Swaps the main display panel to this items panel
     /// </summary>
     public void Show()
     {
-        Debug.Log("RAN!");
         mainCanvas.HideAllPanels();
+        mainCanvas.SetTitle(titleMessage);
         panel.SetActive(true);
     }
 }
