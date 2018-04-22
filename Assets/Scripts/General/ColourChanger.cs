ebc2af885dc547b218a23e49c8c630c09cf4e0c0
HomePC <jollypolly889@gmail.com>
2018-04-22 23:27:42 +0100

    Did some setup work for implementing various features

---
+++ Assets/Scripts/General/ColourChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColourChanger : MonoBehaviour {
+
+	private Image imageField;
+
+	void Start(){
+		imageField = GetComponent<Image> ();
+	}
+
+	public void ChangeMyColour(Color _newColour){
+		imageField.color = _newColour;
+	}
+}
---
+++ Assets/Scripts/Login/LogoutScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoutScript : MonoBehaviour {
+
+	[SerializeField]
+	private MainCanvas mainCanvas;
+
+	[SerializeField]
+	private Animator animator;
+
+
+	public void Logout(){
+
+	}
+}
