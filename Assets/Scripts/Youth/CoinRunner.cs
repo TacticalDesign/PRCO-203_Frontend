ec1b198fe8180672d53ac5640bc51cbc5e8e76d6
Toby Smith <tobysmith568@hotmail.co.uk>
2018-04-27 18:17:56 +0100

    Added in flying coins when feedback is viewed

---
+++ Assets/Scripts/Youth/CoinRunner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Coins))]
+public class CoinRunner : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (!Application.isPlaying)
+        {
+            GUILayout.Label("Run the app to be enable running the coins");
+            return;
+        }
+
+        Coins myScript = (Coins)target;
+
+        GUILayout.Label("Is running: " + (myScript.isRunning? "true" : "false"));
+
+        if (GUILayout.Button("Fire!"))
+        {
+            myScript.Fire();
+        }
+    }
+}
---
+++ Assets/Scripts/Youth/Coins.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    ParticleSystem particles;
+    AudioSource sounds;
+
+    [HideInInspector]
+    public bool isRunning = false;
+
+    private void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+        sounds = GetComponent<AudioSource>();
+    }
+
+
+    public void Fire()
+    {
+        if (!particles.isPlaying)
+        {
+            particles.Play();
+            sounds.Play();
+        }
+    }
+
+    private void Update()
+    {
+        isRunning = particles.isPlaying || sounds.isPlaying;
+    }
+}
